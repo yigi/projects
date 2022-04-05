dc9e773a6d0d385754faab6f4670cefacd7fe596
yigi <yigiterkall@gmail.com>
2022-04-05 11:26:31 +0300

    fix

--- projects-node/blog/routes/articles.js
+++ projects-node/blog/routes/articles.js
@@ -1,37 +1,74 @@
-const express = require('express');
-const router = express.Router();
-const Article = require('../models/article');
+const express = require('express')
+const Article = require('./../models/article')
+const router = express.Router()
 
 router.get('/new', (req, res) => {
-    res.render('articles/new', { article: new Article() });
-});
+  console.log('new')
+  res.render('articles/new', { article: new Article() })
+  console.log('new done')
+})
 
-router.get('/:id', async (req, res) => {
-    const article = await Article.findById(req.params.id);
-    if(article == null) {
-        res.redirect('/');
-    } else {
-    res.render('articles/show', { article: article });
-    }
-});
+router.get('/edit/:id', async (req, res) => {
+  const article = await Article.findById(req.params.id)
+  console.log(article)
+  res.render('articles/edit', { article: article })
+})
 
-router.post('/', async (req, res) => {
-    let article = new Article(
-        {
-            title: req.body.title,
-            description: req.body.description,
-            markdown: req.body.markdown
-        }    
-    );
+router.get('/:slug', async (req, res) => {
+  console.log("try")
+  const article = await Article.findOne({ slug: req.params.slug })
+  if (article == null) res.redirect('/')
+  // console.log(article)
+  res.render('articles/show', { article: article })
+})
+
+router.post('/', async (req, res, next) => {
+  req.article = new Article()
+  next()
+}, saveArticleAndRedirect('new'))
+
+router.put('/:id', async (req, res, next) => {
+  req.article = await Article.findById(req.params.id)
+  next()
+}, saveArticleAndRedirect('edit'))
+
+// method="DELETE"
+router.delete('/:id', async (req, res) => {
+  await Article.findByIdAndDelete(req.params.id)
+  res.redirect('/')
+})
+
+function saveArticleAndRedirect(path) {
+  return async (req, res) => {
+    let article = req.article
+    article.title = req.body.title
+    article.description = req.body.description
+    article.markdown = req.body.markdown
+    article.sanitizedHtml = req.body.markdown
     try {
-        article = await article.save();
-        res.redirect(`/articles/${article._id}`);
-    } catch (error) {
-        console.log(error);
-        res.render('articles/new', { article: article });
+      console.log('saveArticleAndRedirect: 2')
+      article = await article.save()
+      console.log('saveArticleAndRedirect: 2.1')
+      res.redirect(`/articles/${article.slug}`)
+      console.log('saveArticleAndRedirect: 2.2')
+    } catch (e) {
+      console.log('saveArticleAndRedirect: 3.1')
+      console.log(e) 
+      res.render(`articles/${path}`, { article: article })
     }
-   
-});
- 
+  }
+}
 
-module.exports = router;
+
+module.exports = router
+
+/*
+router.get('[LINK]', (req, res) => {
+  res.render('[FILENAME]')
+})
+*/
+
+// ? await article.save() => this is asynchronous function so we just wanna make sure we set this up inside of our own async function and we await this article that save 
+
+// ? res.redirect(someURL) is for you want to return a 30x status code (often 302) to the browser and tell the browser to go to a new URL. This is often done on the server as part of a login process and occasionally when the user finds themselves at an old URL that has been changed to something different. res.redirect() should only be used for these types of forced navigation to a different URL and should never be part of any standard rendering process.
+// ? res.render(filename, data) is how one would typically use EJS (or any other template engine plugged into Express) to fill in a template with some data and return the "rendered" HTML page back to the requesting browser so the browser can render it.
--- projects-node/blog/server.js
+++ projects-node/blog/server.js
@@ -1,24 +1,23 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const Article = require('./models/article');
-const articleRouter = require('./routes/articles');
-const app = express();
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express()
 
 mongoose.connect('mongodb://localhost:27017/blog', { 
     useNewUrlParser: true,
     useUnifiedTopology: true });
-    
-app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
-app.get('/', (_, res) => {
-    const articles = Article.find().sort({ createdAt: 'desc' });
-    res.render('articles/index', { articles: articles} );
-});
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
 
-app.use('/articles', articleRouter);
+app.use('/articles', articleRouter)
 
-app.listen(3000, () => {
-console.log('Server is running on port 3000');
-});
+app.listen(5000)
